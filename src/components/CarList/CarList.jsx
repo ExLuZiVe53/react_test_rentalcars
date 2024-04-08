35e7a99eceff4366cb8e5ffd1105547c07da99c3
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-08 11:12:02 +0200

    corect with link with image cars.json

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,66 +1,36 @@
 import CarCard from "../CarCard/CarCard.jsx";
 
 const CarList = ({ items }) => {
+  console.log(items);
+  const showCars = Array.isArray(items) && items.length;
   return (
-    <ul>
-      {items.map((item) => {
-        return (
-          <li key={`${item.id}_${item.type}`}>
-            {item.model}
-            <CarCard
-              year={item.year}
-              make={item.make}
-              model={item.model}
-              type={item.type}
-              img={item.img}
-              description={item.description}
-              fuelConsumption={item.fuelConsumption}
-              engineSize={item.engineSize}
-              accessories={item.accessories}
-              functionalities={item.functionalities}
-              rentalPrice={item.rentalPrice}
-              rentalCompany={item.rentalCompany}
-              address={item.address}
-              rentalConditions={item.rentalConditions}
-              mileage={item.mileage}
-            />
-          </li>
-        );
-      })}
+    <ul className="carsList">
+      {showCars &&
+        items.map((item) => {
+          return (
+            <li key={`${item.id}_${item.type}`}>
+              {item.model}
+              <CarCard
+                year={item.year}
+                make={item.make}
+                model={item.model}
+                type={item.type}
+                img={item.img}
+                description={item.description}
+                fuelConsumption={item.fuelConsumption}
+                engineSize={item.engineSize}
+                accessories={item.accessories}
+                functionalities={item.functionalities}
+                rentalPrice={item.rentalPrice}
+                rentalCompany={item.rentalCompany}
+                address={item.address}
+                rentalConditions={item.rentalConditions}
+                mileage={item.mileage}
+              />
+            </li>
+          );
+        })}
     </ul>
   );
-  // const showCars = Array.isArray(items) && items.length;
-  // return (
-  //   <ul className="carsList">
-  //     {showCars &&
-  //       items.map((item) => {
-  //         return (
-  //           <li key={item.id}>
-  //             {item.year}
-  //             <CarCard
-  //               id={item.id}
-  // year={item.year}
-  // make={item.make}
-  // model={item.model}
-  // type={item.type}
-  // img={item.img}
-  // description={item.description}
-  // fuelConsumption={item.fuelConsumption}
-  // engineSize={item.engineSize}
-  // accessories={item.accessories}
-  // functionalities={item.functionalities}
-  // rentalPrice={item.rentalPrice}
-  // rentalCompany={item.rentalCompany}
-  // address={item.address}
-  // rentalConditions={item.rentalConditions}
-  // mileage={item.mileage}
-  //             />
-  //           </li>
-  //         );
-  //       })}
-  //   </ul>
-  // );
 };
 export default CarList;
-
-// console.log(items);
